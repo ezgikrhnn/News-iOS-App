04a03bb3955c6714e754c62e35c2fa279cd2cad1
Ezgi Karahan <karahanezgi64@gmail.com>
2024-05-08 14:10:57 +0300

    Initial Commit

---
+++ ACNewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ACNewsApp
+//
+//  Created by Ezgi Karahan on 8.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
