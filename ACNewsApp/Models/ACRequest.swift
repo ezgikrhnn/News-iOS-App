ddcc351b64d2a25eb519abb62a935d2d667bf3de
Ezgi Karahan <karahanezgi64@gmail.com>
2024-05-14 01:27:55 +0300

    command lines edited.

--- ACNewsApp/Models/ACRequest.swift
+++ ACNewsApp/Models/ACRequest.swift
@@ -8,22 +8,28 @@
 import Foundation
 
 final class ACRequest {
+    //hata yönetimi için geri çağırma fonksiyonu
     var onErrorOccurred: ((String) -> Void)?
-
+    
+    ///   - urlString: Haber API uç noktasının URL dizesi.
+    ///   - completion: Veri alındıktan veya bir hata oluştuğunda çalıştırılacak geri çağırma.
     func performRequest(with urlString: String, completion: @escaping (Result<NewsResponse, Error>) -> Void) {
         guard let url = URL(string: urlString) else {
             onErrorOccurred?("Invalid URL")
             return
         }
 
+        // urlden veri cekmek için bir task oluştur:
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            //oluşan hatalar yönet
             if let error = error {
                 DispatchQueue.main.async {
                     self.onErrorOccurred?("Error fetching news: \(error.localizedDescription)")
                 }
                 return
             }
-
+            
+            //geçerli verilerin olup olmadığını kontrol et
             guard let data = data, !data.isEmpty else {
                 DispatchQueue.main.async {
                     self.onErrorOccurred?("No data received")
@@ -31,6 +37,7 @@ final class ACRequest {
                 return
             }
 
+            //json verilerini verilen modele dönüştür
             do {
                 let decoder = JSONDecoder()
                 decoder.dateDecodingStrategy = .iso8601
@@ -38,11 +45,12 @@ final class ACRequest {
                 completion(.success(newsResponse))
 
             } catch {
+                //dönüştürme sırasında oluşan hataları yakala yönet:
                 DispatchQueue.main.async {
                     self.onErrorOccurred?("Error decoding JSON: \(error.localizedDescription)")
                 }
             }
         }
-        task.resume()
+        task.resume() //ağ görevini başlat
     }
 }
